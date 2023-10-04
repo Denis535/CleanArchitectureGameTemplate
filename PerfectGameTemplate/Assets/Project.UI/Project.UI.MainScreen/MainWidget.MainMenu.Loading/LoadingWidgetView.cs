3b3c4c133fbb00a44bd97cc606d157d1cfae9a35
Denis <dddenisss35@gmail.com>
2023-10-04 10:26:13 +0300

    Loading

--- PerfectGameTemplate/Assets/Project.UI/Project.UI.MainScreen/MainWidget.MainMenu.Loading/LoadingWidgetView.cs
+++ PerfectGameTemplate/Assets/Project.UI/Project.UI.MainScreen/MainWidget.MainMenu.Loading/LoadingWidgetView.cs
@@ -43,18 +43,20 @@ namespace Project.UI.MainScreen {
             animation.easingCurve = Easing.Linear;
             animation.valueUpdated = (label, t) => ((Label) label).text = GetLoadingText( t );
             animation.from = 0;
-            animation.to = 60 * 60;
-            animation.durationMs = 60 * 60 * 1000;
+            animation.to = 60;
+            animation.durationMs = 60 * 1000; // 60 seconds
             animation.Start();
         }
         private static string GetLoadingText(float t) {
             var builder = new StringBuilder();
             var text = "Loading...";
             for (var i = 0; i < text.Length; i++) {
-                var a = Mathf.LerpUnclamped( 0f, 0.03f, i + t * 30f );
+                var i01 = (float) i / (text.Length - 1);
+                var a = Mathf.LerpUnclamped( 0f, 0.75f, i01 - t * 1.5f );
                 a = Mathf.PingPong( a, 1 );
-                a = Mathf.Pow( a, 3f );
-                var color = ColorUtility.ToHtmlStringRGBA( new Color( 1, 1, 1, a ) );
+                a = Mathf.Pow( a, 3 );
+                a = Mathf.LerpUnclamped( 0.05f, 1f, a );
+                var color = ColorUtility.ToHtmlStringRGBA( new Color( 0.9f, 0.9f, 0.9f, a ) );
                 var @char = text[ i ];
                 builder.Append( $"<color=#{color}>{@char}</color>" );
             }
