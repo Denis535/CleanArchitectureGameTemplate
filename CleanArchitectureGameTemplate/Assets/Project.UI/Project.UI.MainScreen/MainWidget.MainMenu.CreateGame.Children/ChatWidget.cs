b79fd0f984a563e67b152dca4925f17a8104bdcc
Denis <dddenisss35@gmail.com>
2024-01-30 20:01:12 +0200

    ChatWidget

--- CleanArchitectureGameTemplate/Assets/Project.UI/Project.UI.MainScreen/MainWidget.MainMenu.CreateGame.Children/ChatWidget.cs
+++ CleanArchitectureGameTemplate/Assets/Project.UI/Project.UI.MainScreen/MainWidget.MainMenu.CreateGame.Children/ChatWidget.cs
@@ -29,7 +29,7 @@ namespace Project.UI.MainScreen {
         public override void OnAfterAttach() {
             base.OnAfterAttach();
             for (var i = 1; i <= 32; i++) {
-                View.Messages.Add( Factory.MessageItem( $"Message: {i}", View.Messages.Children.Count ) );
+                View.Messages.Add( Factory.MessageItem( $"Message: {View.Messages.Children.Count + 1}", View.Messages.Children.Count ) );
             }
         }
         public override void OnBeforeDetach() {
@@ -42,7 +42,7 @@ namespace Project.UI.MainScreen {
         private static ChatWidgetView CreateView(UIFactory factory) {
             var view = new ChatWidgetView( factory );
             view.Send.OnClick( () => {
-                view.Messages.Add( factory.MessageItem( view.Text.Value, view.Messages.Children.Count ) );
+                view.Messages.Add( factory.MessageItem( view.Text.Value!, view.Messages.Children.Count ) );
                 view.Text.Value = null;
             } );
             return view;
--- CleanArchitectureGameTemplate/Assets/Project.UI/Project.UI/UIVisual/UIFactoryExtensions.cs
+++ CleanArchitectureGameTemplate/Assets/Project.UI/Project.UI/UIVisual/UIFactoryExtensions.cs
@@ -1,3 +1,4 @@
+#nullable enable
 namespace Project.UI {
     using System;
     using System.Collections;
