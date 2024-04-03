a06d9556704619dad40e54fef7c54cf7e4fd3e9b
Denis <dddenisss35@gmail.com>
2024-04-03 10:39:42 +0300

    Deps

--- CleanArchitectureGameTemplate/Assets/Project.Common/UnityEngine.AddressableAssets/R.cs
+++ CleanArchitectureGameTemplate/Assets/Project.Common/UnityEngine.AddressableAssets/R.cs
@@ -1,55 +1,55 @@
 namespace UnityEngine.AddressableAssets {
     public static class @R {
         public static class @UnityEngine {
-            public const string @LightingSettings = "UnityEngine/LightingSettings.lighting";
+            public const string @LightingSettings_Value = "UnityEngine/LightingSettings.lighting";
             public static class @Audio {
-                public const string @AudioMixer = "UnityEngine.Audio/AudioMixer.mixer";
-                public static class @AudioMixer_SubAssets {
-                    public const string @Game = "UnityEngine.Audio/AudioMixer.mixer[Game]";
-                    public const string @Master = "UnityEngine.Audio/AudioMixer.mixer[Master]";
-                    public const string @Music = "UnityEngine.Audio/AudioMixer.mixer[Music]";
-                    public const string @Sfx = "UnityEngine.Audio/AudioMixer.mixer[Sfx]";
-                    public const string @Snapshot = "UnityEngine.Audio/AudioMixer.mixer[Snapshot]";
+                public const string @AudioMixer_Value = "UnityEngine.Audio/AudioMixer.mixer";
+                public static class @AudioMixer {
+                    public const string @Game_Value = "UnityEngine.Audio/AudioMixer.mixer[Game]";
+                    public const string @Master_Value = "UnityEngine.Audio/AudioMixer.mixer[Master]";
+                    public const string @Music_Value = "UnityEngine.Audio/AudioMixer.mixer[Music]";
+                    public const string @Sfx_Value = "UnityEngine.Audio/AudioMixer.mixer[Sfx]";
+                    public const string @Snapshot_Value = "UnityEngine.Audio/AudioMixer.mixer[Snapshot]";
                 }
             }
         }
         public static class @Project {
             public static class @UI {
-                public const string @PanelSettings = "Project.UI/PanelSettings.asset";
+                public const string @PanelSettings_Value = "Project.UI/PanelSettings.asset";
                 public static class @MainScreen {
                     public static class @Images {
-                        public const string @Background = "Project.UI.MainScreen/Images/Background.png";
-                        public static class @Background_SubAssets {
-                            public const string @Background = "Project.UI.MainScreen/Images/Background.png[Background]";
+                        public const string @Background_Value = "Project.UI.MainScreen/Images/Background.png";
+                        public static class @Background {
+                            public const string @Background_Value = "Project.UI.MainScreen/Images/Background.png[Background]";
                         }
                     }
                     public static class @Music {
-                        public const string @Theme = "Project.UI.MainScreen.Music.Theme.wav";
+                        public const string @Theme_Value = "Project.UI.MainScreen.Music.Theme.wav";
                     }
                 }
                 public static class @GameScreen {
                     public static class @Music {
-                        public const string @Theme_1 = "Project.UI.GameScreen.Music.Theme_1.wav";
-                        public const string @Theme_2 = "Project.UI.GameScreen.Music.Theme_2.wav";
+                        public const string @Theme_1_Value = "Project.UI.GameScreen.Music.Theme_1.wav";
+                        public const string @Theme_2_Value = "Project.UI.GameScreen.Music.Theme_2.wav";
                     }
                 }
                 public static class @StyleSheets {
-                    public const string @ThemeStyleSheet = "Project.UI/StyleSheets/ThemeStyleSheet.uss";
+                    public const string @ThemeStyleSheet_Value = "Project.UI/StyleSheets/ThemeStyleSheet.uss";
                 }
                 public static class @ThemeStyleSheets {
-                    public const string @ThemeStyleSheet = "Project.UI/ThemeStyleSheets/ThemeStyleSheet.tss";
+                    public const string @ThemeStyleSheet_Value = "Project.UI/ThemeStyleSheets/ThemeStyleSheet.tss";
                 }
             }
             public static class @Entities {
                 public static class @Worlds {
-                    public const string @World_01 = "Project.Entities.Worlds/World_01.unity";
-                    public const string @World_02 = "Project.Entities.Worlds/World_02.unity";
+                    public const string @World_01_Value = "Project.Entities.Worlds/World_01.unity";
+                    public const string @World_02_Value = "Project.Entities.Worlds/World_02.unity";
                 }
             }
             public static class @Scenes {
-                public const string @Program = "Project.Scenes/Program.unity";
-                public const string @MainScene = "Project.Scenes/MainScene.unity";
-                public const string @GameScene = "Project.Scenes/GameScene.unity";
+                public const string @Program_Value = "Project.Scenes/Program.unity";
+                public const string @MainScene_Value = "Project.Scenes/MainScene.unity";
+                public const string @GameScene_Value = "Project.Scenes/GameScene.unity";
             }
         }
     }
